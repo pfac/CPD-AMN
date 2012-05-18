28839cba6abad3c04849268224a8a4bb080035d9
Pedro Costa <dev@pfac.info>
2012-05-18 18:40:18 +0100

    MPI fixed

--- distribute.c
+++ distribute.c
@@ -182,37 +182,89 @@ int main ( int argc , char * argv[] ) {
 	uint   nstudents;
 	uint * rooms;
 	int    mpi_rank;
+	int    mpi_size;
 
-	//0. Read arguments
-	if ( argc > 1 )
-		nstudents = parse_uint( argv[1] );
-
-	//0. Preparation
-	laststudent = nstudents - 1;
-
-	//0. Create arrays
-	dislikes = (uint *) malloc( nstudents * nstudents * sizeof( uint ) );
-	rooms    = (uint *) malloc( nstudents             * sizeof( uint ) );
-
-	//0. Generate dislike matrices
-	random_dislikes( dislikes , nstudents , 1 , 10 );
-
-	//0. Add perfect solution
-	for ( i = 0 ; i < laststudent ; i += 2 )
-	{
-		j = i + 1;
-		dislikes[ i * nstudents + j ] = 0;
-		dislikes[ j * nstudents + i ] = 0;
-	}
 
 	MPI_Init( &argc , &argv );
 	MPI_Comm_rank(MPI_COMM_WORLD,&mpi_rank);
+	MPI_Comm_size (MPI_COMM_WORLD, &mpi_size);
+
+	if (!mpi_rank)
+	{
+		//0. Read arguments
+		if ( argc > 1 )
+			nstudents = parse_uint( argv[1] );
+
+		// //0. Preparation
+		// laststudent = nstudents - 1;
+
+		// //0. Generate dislike matrices
+		// random_dislikes( dislikes , nstudents , 1 , 10 );
+
+		// //0. Add perfect solution
+		// for ( i = 0 ; i < laststudent ; i += 2 )
+		// {
+		// 	j = i + 1;
+		// 	dislikes[ i * nstudents + j ] = 0;
+		// 	dislikes[ j * nstudents + i ] = 0;
+		// }
+	}
+	MPI_Bcast (&nstudents, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+	laststudent = nstudents - 1;
+
+	dislikes = (uint *) malloc( nstudents * nstudents * sizeof( uint ) );
+
+	if (!mpi_rank)
+	{
+		//0. Generate dislike matrices
+		random_dislikes( dislikes , nstudents , 1 , 10 );
+
+		//0. Add perfect solution
+		for ( i = 0 ; i < laststudent ; i += 2 )
+		{
+			j = i + 1;
+			dislikes[ i * nstudents + j ] = 0;
+			dislikes[ j * nstudents + i ] = 0;
+		}
+	}
+	MPI_Bcast (dislikes, nstudents * nstudents, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+
+	//0. Create arrays
+	rooms = (uint*) malloc (nstudents * sizeof (uint));
+
+		// //debug
+		// for (int r = 0; r < mpi_size; ++r)
+		// {
+		// 	MPI_Barrier (MPI_COMM_WORLD);
+		// 	if (r == mpi_rank)
+		// 	{
+		// 		fprintf (stderr, "%lu\n", nstudents);
+		// 		for (i = 0; i < nstudents; ++i)
+		// 		{
+		// 			for (j = 0; j < nstudents; ++j)
+		// 				fprintf (stderr, "%lu ", dislikes[i * nstudents + j]);
+		// 			fprintf (stderr, "\n");
+		// 		}
+		// 	}
+		// 	MPI_Barrier (MPI_COMM_WORLD);
+		// }
 	
 	//1. No simulated annealing
 	lc1 = distribute1( dislikes , rooms , nstudents );
 
 	//2. Now with simulated annealing
 	lc2 = distribute2( dislikes , rooms , nstudents );
+
+		// //debug
+		// for (int r = 0; r < mpi_size; ++r)
+		// {
+		// 	MPI_Barrier (MPI_COMM_WORLD);
+		// 	if (r == mpi_rank)
+		// 	{
+		// 		fprintf (stderr, "%ld %ld\n", lc1, lc2);
+		// 	}
+		// 	MPI_Barrier (MPI_COMM_WORLD);
+		// }
 
 	MPI_Reduce( &lc1 , &c1 , 1, MPI_UNSIGNED_LONG , MPI_MIN , 0 , MPI_COMM_WORLD );
 	MPI_Reduce( &lc2 , &c2 , 1, MPI_UNSIGNED_LONG , MPI_MIN , 0 , MPI_COMM_WORLD );
@@ -232,7 +284,7 @@ int main ( int argc , char * argv[] ) {
 
 	// 99. Cleanup
 	free( rooms );
-	free( dislikes );
+	free (dislikes);
 	
 	return 0;
 }
