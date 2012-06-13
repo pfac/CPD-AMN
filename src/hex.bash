78377f8c94f198a9d9184698b9325809e220cfb4
Pedro Costa <dev@pfac.info>
2012-06-13 14:21:55 +0100

    Changed PBS scripts to test initial temperature

--- src/hex.bash
+++ src/hex.bash
@@ -9,17 +9,30 @@
 
 EXEC="distribute"
 
-NSTART=40
-NSTOP=200
-NSTEP=4
+if [ ! "$NFIRST" ]; then NFIRST="40"; fi;
+if [ ! "$NLAST"  ]; then NLAST="200"; fi;
+if [ ! "$NSTEP"  ]; then NSTEP="4";   fi;
+
+if [ ! "$TFIRST" ]; then TFIRST="1"; fi;
+if [ ! "$TLAST"  ]; then TLAST="1"; fi;
+if [ ! "$TSTEP"  ]; then TSTEP="1"; fi;
+
+echo "N from ${NFIRST} to ${NLAST} by ${NSTEP}";
+echo "T from ${TFIRST} to ${TLAST} by ${TSTEP}";
 
 cd "$PBS_O_WORKDIR";
 PROCESSES=`cat "$PBS_NODEFILE" | wc -l`;
 
-N=$NSTART;
-while [ "$N" -le "$NSTOP" ];
+N="$NFIRST";
+while [ "$N" -le "$NLAST" ];
 do
-	#mpirun -np $PROCESSES -machinefile $PBS_NODEFILE -loadbalance $EXEC "$N";
-	mpirun -np 4 -machinefile $PBS_NODEFILE -loadbalance $EXEC "$N";
+	T="$TFIRST";
+	while [ "$T" -le "$TLAST" ];
+	do
+		echo -n "(${N},${T})..." 1>&2;
+		mpirun -np "$PROCESSES" -machinefile "$PBS_NODEFILE" -loadbalance "$EXEC" "$N" "$T";
+		echo "DONE" 1>&2;
+		T=$(( $T + $TSTEP ));
+	done;
 	N=$(( $N + $NSTEP ));
 done;
