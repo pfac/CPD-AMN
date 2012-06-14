4fc7d876b91b707133d58fa84e6f08ccf42d44fc
Pedro Costa <dev@pfac.info>
2012-06-14 19:19:38 +0100

    Added alternative random number generator

---
+++ src/arc4.c
@@ -0,0 +1,33 @@
+#include "archrand.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+
+void randomseed (unsigned int seed) {srand(seed);}
+
+#ifndef _LP64
+real randomr () {
+	real f = (real) arc4random() / (real) UINT32_MAX;
+	return f;
+}
+#else
+union _32to64 {
+	uint32_t u32[2];
+	uint64_t u64;
+};
+
+real randomr () {
+	union _32to64 r;
+	r.u32[0] = arc4random();
+	r.u32[1] = arc4random();
+	real f = (real) r.u64 / (real) UINT64_MAX;
+	return f;
+}
+#endif
+
+unsigned long randomul_lim (unsigned long min ,unsigned long max) {
+	real f = randomr();
+	return f * ( max - min + 1 ) + min;
+}
